8ab68facedbad9820627cc4a292b206726a46b38
Luca Dovichi <lucadovichi@gmail.com>
2023-09-07 09:59:21 -0700

    this is becoming embarassing

--- src/config/SpotifyApiConfig.js
+++ src/config/SpotifyApiConfig.js
@@ -1,5 +1,5 @@
 export const CLIENT_ID = "d8bc475ca4454c5197562bca87b69626"
-export const REDIRECT_URI = "loopster.l-uca.com/redirect"
+export const REDIRECT_URI = "http://loopster.l-uca.com/redirect"
 export const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
 export const RESPONSE_TYPE = "token"
 export const SCOPE = "playlist-modify-private user-library-read"
