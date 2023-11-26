859a67e35ba25b34f030a6c4a97879aceb5d04aa
Luca Dovichi <lucadovichi@gmail.com>
2023-11-26 13:21:45 -0600

    quick fix

--- src/config/SpotifyApiConfig.js
+++ src/config/SpotifyApiConfig.js
@@ -1,4 +1,4 @@
-export const CLIENT_ID = "d8bc475ca4454c5197562bca87b69626";
+export const CLIENT_ID = "";
 export const REDIRECT_URI = "http://localhost:3000/redirect";
 export const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 export const RESPONSE_TYPE = "token";
