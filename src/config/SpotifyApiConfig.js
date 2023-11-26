2725a6854015e1558837e00b28cf88d1d08e1397
Luca Dovichi <lucadovichi@gmail.com>
2023-11-26 13:27:31 -0600

    fixing redirect url

--- src/config/SpotifyApiConfig.js
+++ src/config/SpotifyApiConfig.js
@@ -1,5 +1,5 @@
 export const CLIENT_ID = "d8bc475ca4454c5197562bca87b69626";
-export const REDIRECT_URI = "http://localhost:3000/redirect";
+export const REDIRECT_URI = "http://loopster.l-uca.com.com/redirect";
 export const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 export const RESPONSE_TYPE = "token";
 export const SCOPE = "playlist-modify-private user-library-read";
