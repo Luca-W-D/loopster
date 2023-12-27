32525829766f8fc130c3cf662b032406cd4d95ed
Luca Dovichi <lucadovichi@gmail.com>
2023-12-26 16:20:42 -0800

    added content displayed to music player when preview unavailable

--- src/elements/Player.jsx
+++ src/elements/Player.jsx
@@ -3,11 +3,16 @@ import { useState } from "react";
 import Card from "../components/Card";
 import MusicViewer from "./MusicViewer";
 import QueueViewer from "./QueueViewer";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 
-export default function Player({ sessions, numberOfSeeks, smartSeek, setRequest, currentTrack, setCurrentTrack, currentSession, setCurrentSession, songName, request, currentSong }) {
+export default function Player({ sessions, numberOfSeeks, smartSeek, setRequest, currentTrack, setCurrentTrack, currentSession, setCurrentSession, songName, request, currentSong, notificationText }) {
     return <Card title="Preview Music" hideTitle={true} removePadding={true}>
         <div className="flex flex-row h-full items-center p-6">
-            {currentSong ? <MusicViewer name={currentSong.name} artist={currentSong.artist} art={currentSong.art} /> : <MusicViewer />}
+            {notificationText ? <div className="flex-grow flex flex-col text-gray-300 font-semibold text-center">
+                <ExclamationTriangleIcon className="h-24" />
+                <p className="font-sm">Content Unavailable</p>
+                </div> :
+            currentSong ? <MusicViewer name={currentSong.name} artist={currentSong.artist} art={currentSong.art} /> : <MusicViewer />}
             <QueueViewer numberOfSeeks={numberOfSeeks} smartSeek={smartSeek} currentTrack={currentTrack} setCurrentTrack={setCurrentTrack} setRequest={setRequest} currentSession={currentSession} songName={songName} request={request} setCurrentSession={setCurrentSession} sessions={sessions} />
         </div>
     </Card>
